5f4e93a11bcec11d4c23b88e1b148b5a16cff894
Naimul Ferdous <ferdousnaimul@gmail.com>
2021-01-10 21:56:01 +0600

    cursor style changed in image zoom

--- src/components/Footer/LinkLists/LinkList.tsx
+++ src/components/Footer/LinkLists/LinkList.tsx
@@ -4,7 +4,6 @@ import styled from "styled-components";
 
 const LinkList = ({ header, lists }) => {
   const history = useHistory();
-  console.log({ lists });
 
   return (
     <TopFooterContainer>
